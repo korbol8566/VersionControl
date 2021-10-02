dbe14dc4466b7236d289401392fa8c57c331f334
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-02 20:18:17 +0200

    7. Adatok exportálása

--- IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01/Form1.cs
+++ IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01/Form1.cs
@@ -27,8 +27,53 @@ namespace IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01
             InitializeComponent();
             LoadData();
             CreateExcel();
+            CreateTable();
         }
 
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m^2)",
+                "Ár (MFt)",
+                "Négyzetméterár (Ft/m^2)"
+            };
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                // ...
+                values[counter, 8] = "";
+                counter++;
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
         private void CreateExcel()
         {
             try
@@ -43,7 +88,7 @@ namespace IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-                //CreateTable(); // Ennek megírása a következő feladatrészben következik
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
