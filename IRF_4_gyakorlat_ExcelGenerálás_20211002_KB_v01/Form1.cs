0e6c84ea96262ba271e5e5da5b94ba72e56f17b0
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-02 20:03:29 +0200

    4. Adatok betöltése

--- IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01/Form1.cs
+++ IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01/Form1.cs
@@ -12,9 +12,18 @@ namespace IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
