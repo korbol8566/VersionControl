80e774808e5c98e7a7b100244d711ea7f84501a7
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-02 20:10:23 +0200

    6. Excel munkafüzet létrehozása és megnyitása

--- IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01/Form1.cs
+++ IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01/Form1.cs
@@ -18,13 +18,49 @@ namespace IRF_4_gyakorlat_ExcelGenerálás_20211002_KB_v01
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
-
+        Excel.Application xlApp;
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+        }
 
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                //CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null; ;
+            }
         }
 
         private void LoadData()
