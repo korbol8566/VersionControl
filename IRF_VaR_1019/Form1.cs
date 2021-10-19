a2544ccc1c94f29c2ba480c4a5fb979b026027a0
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-19 19:38:41 +0200

    5. Portfólió tárolása a memóriában

--- IRF_VaR_1019/Form1.cs
+++ IRF_VaR_1019/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_VaR_1019.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,12 +15,27 @@ namespace IRF_VaR_1019
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>(); //using...
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+            
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            // így is lehetne 
+            //PortfolioItem p = new PortfolioItem();
+            //p.Index = "OTP";
+            //p.Volume = 10;
+            //Portfolio.Add(p);
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
