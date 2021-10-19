165709c28c76d19bee79aaff18e64de0d167d605
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-19 19:29:39 +0200

    4. Context, list, DGV ready

--- IRF_VaR_1019/Form1.cs
+++ IRF_VaR_1019/Form1.cs
@@ -12,9 +12,14 @@ namespace IRF_VaR_1019
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
