4de81454cc3bf17c968ff952617666e6394a039b
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-24 16:25:43 +0200

    1. Webservice Call

---
+++ IRF_SOAP_MNB_1024/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_SOAP_MNB_1024.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal  Value { get; set; }
+    }
+}
--- IRF_SOAP_MNB_1024/Form1.cs
+++ IRF_SOAP_MNB_1024/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IRF_SOAP_MNB_1024.Entities;
+using IRF_SOAP_MNB_1024.MNBServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,32 @@ namespace IRF_SOAP_MNB_1024
 {
     public partial class Form1 : Form
     {
+        //névtér
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        
         public Form1()
         {
             InitializeComponent();
+            WebserviceCall();
+            
+
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void WebserviceCall()
+        {
+            //kell előtte az a namespace: using IRF_SOAP_MNB_1024.MNBServiceReference;
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
