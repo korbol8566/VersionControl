675bcd8b8d8c70d21e3046e9fd13d29577fd5f22
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-31 20:56:25 +0100

    6. Car

---
+++ IRF_Patterns_1031/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using IRF_Patterns_1031.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Patterns_1031.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
