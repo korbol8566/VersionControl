6da08c6e7443aff785a6c21e3dfab97082116f7c
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-31 20:23:14 +0100

    2. Ball factory created

---
+++ IRF_Patterns_1031/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Patterns_1031.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
--- IRF_Patterns_1031/Form1.cs
+++ IRF_Patterns_1031/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_Patterns_1031.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace IRF_Patterns_1031
 {
     public partial class Form1 : Form
     {
+        List<Ball> list_balls = new List<Ball>();
         public Form1()
         {
             InitializeComponent();
