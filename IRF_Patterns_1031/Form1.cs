43828796c91cafbf7fbb40ba25744fcd0d0f8f01
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-10-31 20:33:21 +0100

    3. Conveyor created

--- IRF_Patterns_1031/Form1.cs
+++ IRF_Patterns_1031/Form1.cs
@@ -14,9 +14,41 @@ namespace IRF_Patterns_1031
     public partial class Form1 : Form
     {
         List<Ball> list_balls = new List<Ball>();
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var Ball = Factory.CreateNew();
+            list_balls.Add(Ball);
+            Ball.Left = -Ball.Width;
+            panel1.Controls.Add(Ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in list_balls)
+            {
+                ball.MoveBall();
+                if (ball.Left>maxPosition) maxPosition = ball.Left;
+            }
+            if (maxPosition>1000)
+            {
+                var oldestBall = list_balls[0];
+                panel1.Controls.Remove(oldestBall);
+                list_balls.Remove(oldestBall);
+            }
         }
     }
 }
