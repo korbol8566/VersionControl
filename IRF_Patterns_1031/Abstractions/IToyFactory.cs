6cc25f97630f05abe9bf95b16026457bfacec1f8
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-11-01 08:59:09 +0100

    7. Gyár absztrakció kezelése

---
+++ IRF_Patterns_1031/Abstractions/IToyFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Patterns_1031.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
