675748eab6692aedc9b59934a60845acaca1c431
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-11-23 12:47:09 +0100

    1. E-mail validálás teszt függvény

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test] // find and install
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            
+        }
+    }
+}
---
+++ UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa20eaba-8941-4066-88a3-ed6151f31a40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
