a66f46232e68f42017ff585c77519ac7b9760b4a
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-11-30 09:22:51 +0100

    4. Szimuláció

--- IRF_microsim_20211129/IRF_microsim_20211129/Form1.cs
+++ IRF_microsim_20211129/IRF_microsim_20211129/Form1.cs
@@ -18,7 +18,7 @@ namespace IRF_microsim_20211129
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
-
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
@@ -26,7 +26,23 @@ namespace IRF_microsim_20211129
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
-            Random rng = new Random(1234);
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
