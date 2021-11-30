eb17a43f381179956863d2a9cf3e7f17b889905a
korbol8655 <imre.kormendy@stud.uni-corvinus.hu>
2021-11-30 09:16:31 +0100

    1. Osztályok létrehozása

---
+++ IRF_microsim_20211129/IRF_microsim_20211129/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_microsim_20211129.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+        public int childrenCount { get; set; }
+        public double deathProbability { get; set; }
+    }
+}
---
+++ IRF_microsim_20211129/IRF_microsim_20211129/Entities/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_microsim_20211129.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
